fff8012e56f561f5baba7a34bf5b8e0a7e823d48
Tiago Costa <tiagodpv@gmail.com>
2023-01-02 00:09:38 -0500

    last

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import ImageSlider from './components/ImageSlider';
 import Navbar from './components/Navbar';
@@ -14,6 +15,7 @@ function App() {
       <Plan />
       <Rooms />
       <ImageSlider />
+      <Footer />
     </div>
   );
 }
